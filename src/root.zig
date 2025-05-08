da0880f282f38772b70e005521efdfad2a2b82df
ross-weir <29697678+ross-weir@users.noreply.github.com>
2025-05-09 00:25:42 +1000

    some more stuff

--- src/root.zig
+++ src/root.zig
@@ -11,13 +11,29 @@ pub const Options = struct {
     format: format.Format = .text,
 };
 
+/// A record structure containing information about a logging event.
+/// This type contains fields that are comptime known only.
+///
+/// This struct is separate to `Context` so it can remain `comptime`
+/// which is important for comptime log level checking.
 pub const Record = struct {
     level: std.log.Level,
     scope: @Type(.enum_literal),
 };
 
+/// Provides context that is needed to write the log line.
+/// The extra context could be added depending on option
+/// configuration and will be determined at runtime.
+///
+/// This struct is separate to `Record` because it contains
+/// runtime fields and thus can't be used at comptime.
 pub const Context = struct {
+    /// The formatted log message
     message: []const u8,
+    /// The datetime of the log
+    ///
+    /// Will be available if logging was configured to include
+    /// a timestamp, otherwise it will be null.
     datetime: ?[]const u8 = null,
     // thread id
 };
